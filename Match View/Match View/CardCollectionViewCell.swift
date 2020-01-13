57ceb070f31e218f7b73e34a049d8164abb2cc4f
xo28122000 <jainam.2000@yahoo.com>
2020-01-13 03:14:27 -0800

    lesson 7

--- Match View/Match View/CardCollectionViewCell.swift
+++ Match View/Match View/CardCollectionViewCell.swift
@@ -15,4 +15,43 @@ class CardCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var backImageView: UIImageView!
     
+    var card:Card?
+
+    func setCard(_ card:Card){
+        
+        self.card = card
+        
+        frontImageView.image = UIImage(named: card.imageName)
+        
+        
+        
+        
+//        problem of cell positioning changing
+        
+        if(card.isFlipped){
+             UIView.transition(from: backImageView, to: frontImageView, duration: 0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        }
+        else{
+            
+            UIView.transition(from: frontImageView, to: backImageView, duration: 0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+            
+        }
+        
+        
+//
+        
+        
+    }
+    
+    func flip(){
+        
+        UIView.transition(from: backImageView, to: frontImageView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        
+    }
+    
+    func flipBack(){
+        
+        UIView.transition(from: frontImageView, to: backImageView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        
+    }
 }
--- Match View/Match View/ViewController.swift
+++ Match View/Match View/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     var model = CardModel()
     var cardArray = [Card]()
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         cardArray = model.getards()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+        
     }
 
+    
+    //    MARK: UIcollectionView Protocol methods
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
+        
+        let card = cardArray[indexPath.row]
+        
+        cell.setCard(card)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
+        
+      
+        
+        let card = cardArray[indexPath.row]
+        
+        if(!card.isFlipped){
+              cell.flip()
+            card.isFlipped = true
+        }
+        else{
+            cell.flipBack()
+            card.isFlipped = false
+        }
+        
+    }
+    
 
 }
 
